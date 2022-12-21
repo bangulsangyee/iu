f78f9466c8945fe35bfcfefce90ddb54c520cb7a
강민성 <underfellunder@dgsw.hs.kr>
2022-12-21 16:07:45 +0900

    캐릭터이동과 벽세우기
    캐릭터가 벽에 박으면 캐릭터가 벽 색으로 변하게 만들었습니다.

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -13,9 +13,20 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //float h = Input.GetAxis("Horizontal"); //키보드 왼쪽 오른쪽 입력받음 왼쪽은 -1 오른쪽은 1
-        //print(h);
+        float h = Input.GetAxis("Horizontal"); //키보드 왼쪽 오른쪽 입력받음 왼쪽은 -1 오른쪽은 1
         float v = Input.GetAxis("Vertical"); //키보드 위 아래 입력받음 위는 1 아래쪽은 -1
-        print(v);
+        Vector3 dir = new Vector3(h, v, 0);//가로 세로 설정
+        //transform.position += dir; //해당 좌표로 순간이동되어 엄청 빠름 그래서 벽도 뚫고 지나감
+        GetComponent<Rigidbody2D>().velocity //dir의 값만큼 움직이는건데 *5를 해주므로서 이동속도를 늘림 
+            = dir * 5;  //물지적인 충돌이기 때문에 만약 1이라는 값으로 이동한다면 0에서1로 천천히 이동함
+       
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("WALL")) //만약 벽에 부딪친다면 벽의 색으로 바뀌게 된다.
+        {
+            GetComponent<SpriteRenderer>().color = collision.collider.GetComponent<SpriteRenderer>().color;
+        }
+    }
+
 }
